b1a128cca9e206b67ed1fd185442a9fa138927f1
neal-tommy82 <neal_tommy@yahoo.co.uk>
2016-02-14 10:33:26 +0200

    Modifying Code to Work on Photon

---
+++ Modified Code/HelloWorld.ino
@@ -0,0 +1,31 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "I2C_LCD.h"
+#define pgm_read_byte_near(_ptr) *(uint8_t*)_ptr
+
+I2C_LCD LCD;
+uint8_t I2C_LCD_ADDRESS = 0x51; //Device address configuration, the default value is 0x51.
+
+
+//For detials of the function useage, please refer to "I2C_LCD User Manual". 
+//You can download the "I2C_LCD User Manual" from I2C_LCD WIKI page: http://www.seeedstudio.com/wiki/I2C_LCD
+
+
+void setup()
+{
+    Wire.begin();         //I2C controller initialization.
+    Particle.process();
+}
+
+void loop()
+{
+    LCD.CleanAll(WHITE);    //Clean the screen with black or white.
+    delay(1000);            //Delay for 1s.
+
+    Particle.process();
+
+    //8*16 font size��auto new line��black character on white back ground.
+    LCD.FontModeConf(Font_6x8, FM_ANL_AAA, BLACK_BAC); 
+    LCD.DispStringAt("Hello World!", 0, 10);    //Display "Hello World!" on coordinate of (0, 10).
+
+    //while(1); //Wait for ever. 
+}
